8c8f28b911472f43a873040a77b26c452a5fe6ed
King-Yan Kwan <kykwan@linux.suse>
2016-10-29 22:52:07 +0800

    Experimental linker script\nExtract compiler flags\nEncapsulate headers in anonymous structures to enforce declaration order in binary image

--- Makefile
+++ Makefile
@@ -2,5 +2,8 @@ include ../automation/compiler.mk
 include ../automation/diag.mk 
 include ../automation/lang.mk
 
+LINK_FLAGS = -ffreestanding -nostartfiles -nodefaultlibs -fno-rtti -static -T experiment.ld
+DEBUG_FLAGS = -gsplit-dwarf -g3 -gdwarf-4
+
 bootimagetest: bootimagetest.cpp experiment.ld
-	$(CC) $(LANG_VER) $(DIAG_FLAGS) -ffreestanding -nostartfiles -nodefaultlibs -fno-rtti -static -o bootimagetest -T experiment.ld bootimagetest.cpp
+	$(CC) $(LANG_VER) $(DIAG_FLAGS) $(LINK_FLAGS) $(DEBUG_FLAGS) -o $@ bootimagetest.cpp
--- bootimagetest.cpp
+++ bootimagetest.cpp
@@ -1,32 +1,41 @@
 #include <cstdint>
 
-// AM335x Rev O 26.1.7.5.5.1 p.4988-4989
-volatile unsigned char configuration_header_toc[] = {
-    0,    0,    0,    4,                                        // Start
-    0,    0,    0,    0xC,                                      // Size
-    0,    0,    0,    0,                                        // Reserved
-    0,    0,    0,    0,                                        // Reserved
-    0,    0,    0,    0,                                        // Reserved
-    'C',  'H',  'S',  'E',  'T', 'T', 'I', 'N', 'G', 'S', 0, 0, // Filename
-    0xFF, 0xFF, 0xFF, 0xFF,                                     // Closing item
-    0xFF, 0xFF, 0xFF, 0xFF,                                     // Closing item
-    0xFF, 0xFF, 0xFF, 0xFF,                                     // Closing item
-    0xFF, 0xFF, 0xFF, 0xFF,                                     // Closing item
-    0xFF, 0xFF, 0xFF, 0xFF,                                     // Closing item
-    0xFF, 0xFF, 0xFF, 0xFF,                                     // Closing item
-    0xFF, 0xFF, 0xFF, 0xFF,                                     // Closing item
-    0xFF, 0xFF, 0xFF, 0xFF,                                     // Closing item
-};
+struct {
+  // AM335x Rev O 26.1.7.5.5.1 p.4988-4989
+  const unsigned char configuration_header_toc[0x3C + 4] = {
+      0,    0,    0,    4,                                        // Start
+      0,    0,    0,    0xC,                                      // Size
+      0,    0,    0,    0,                                        // Reserved
+      0,    0,    0,    0,                                        // Reserved
+      0,    0,    0,    0,                                        // Reserved
+      'C',  'H',  'S',  'E',  'T', 'T', 'I', 'N', 'G', 'S', 0, 0, // Filename
+      0xFF, 0xFF, 0xFF, 0xFF, // Closing item
+      0xFF, 0xFF, 0xFF, 0xFF, // Closing item
+      0xFF, 0xFF, 0xFF, 0xFF, // Closing item
+      0xFF, 0xFF, 0xFF, 0xFF, // Closing item
+      0xFF, 0xFF, 0xFF, 0xFF, // Closing item
+      0xFF, 0xFF, 0xFF, 0xFF, // Closing item
+      0xFF, 0xFF, 0xFF, 0xFF, // Closing item
+      0xFF, 0xFF, 0xFF, 0xFF, // Closing item
+  };
 
-// AM335x Rev O 26.1.7.5.5.1 p.4989
-volatile unsigned char configuration_header_settings[] = {
-    0xC0, 0xC0, 0xC0, 0xC1,                // Section keys
-    0,                                     // Valid
-    1,                                     // Version
-    0,    0,    0,    0,    0, 0, 0, 0, 0, // Reserved
-};
+  // AM335x Rev O 26.1.7.5.5.1 p.4989
+  const unsigned char configuration_header_settings[0xe + 1] = {
+      0xC0, 0xC0, 0xC0, 0xC1,                // Section keys
+      0,                                     // Valid
+      1,                                     // Version
+      0,    0,    0,    0,    0, 0, 0, 0, 0, // Reserved
+  };
 
-// AM335x Rev O 26.1.9.2 p.5007
-volatile std::uint32_t size = 42;
-volatile std::uint32_t destination = 43;
-extern "C" void start() { size = size + destination; }
+  // AM335x Rev O 26.1.9.2 p.5007
+  struct {
+    std::uint32_t size = 42;
+    std::uint32_t destination = 43;
+  } gp_header;
+
+} volatile boot_header;
+
+extern "C" void start() {
+  boot_header.gp_header.size =
+      boot_header.gp_header.size + boot_header.gp_header.destination;
+}
