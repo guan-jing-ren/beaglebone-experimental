64397fa841da991c693eff9835ea78ad20758a81
King-Yan Kwan <kykwan@linux.suse>
2016-12-15 13:22:46 +0800

    Cast offset to right type

--- offset_register.hpp
+++ offset_register.hpp
@@ -5,7 +5,8 @@ template <typename E, std::size_t... fields>
 struct offset_register : public fm::memory_mapped_register<E, fields...> {
   template <typename T>
   offset_register(T t)
-      : fm::memory_mapped_register<E, fields...>(E::offset + t) {}
+      : fm::memory_mapped_register<E, fields...>(
+            static_cast<T>(E::offset) + t) {}
 };
 
 #endif
